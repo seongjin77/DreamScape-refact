3767502f3b42091138cae7eec3d1c5d16a2c98dd
gyumin <gnto2000@naver.com>
2024-10-31 00:21:58 +0900

    [fix] 헤더 메뉴바 toggle ,스크롤시 헤더 hide 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
+import GlobalStyle from './common/global-styled';
 
 function App() {
   return (
     <Router>
       <AppRoutes />
+      <GlobalStyle />
     </Router>
   );
 }
--- src/container/Main/index.tsx
+++ src/container/Main/index.tsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MainStyle } from './Styled';
 
 const Main: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0); 
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Main Page</h1>
-      <p>Welcome to the Main Page!</p>
-    </div>
+    <MainStyle>
+      <div className={`header-wrapper ${isScrolled ? 'scrolled' : ''}`}>
+        <div className="menu-item-wrapper">
+          <div className="title-wrapper">
+            <h1>로고</h1>
+          </div>
+
+          <nav className="web-nav-list-wrapper">
+            <ul>
+              <li>best</li>
+              <li>worst</li>
+              <li>top5</li>
+            </ul>
+          </nav>
+          <nav className="mobile-nav-list-wrapper">
+            <div
+              className={`mobile-menu-btn ${isMenuOpen ? 'open' : ''}`}
+              onClick={toggleMenu}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <ul className={`menu-area ${isMenuOpen ? 'open' : ''}`}>
+              <li>best</li>
+              <li>worst</li>
+              <li>top5</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <section>
+        <div className="main-title"></div>
+        <div>
+          <textarea></textarea>
+          <div className="btn-list"></div>
+        </div>
+      </section>
+      <section>
+        <div className="main-title"></div>
+        <div>
+          <textarea></textarea>
+          <div className="btn-list"></div>
+        </div>
+      </section>
+      <footer>
+        <div></div>
+        <div></div>
+      </footer>
+    </MainStyle>
   );
 };
 
