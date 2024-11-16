60cee170a128ed708e37e7c166e955aa7a9c751c
gyumin <gnto2000@naver.com>
2024-11-17 00:56:37 +0900

    [fix] 브랜치 생성

--- src/components/AspectRatioSelector/index.tsx
+++ src/components/AspectRatioSelector/index.tsx
@@ -1,14 +1,15 @@
+import { SelectorStyle } from './Styled';
 interface OptionProps {
   setAspectRatio: (newAspectRatio: string) => void;
 }
 
 const Option: React.FC<OptionProps> = ({ setAspectRatio }) => {
   return (
-    <div>
+    <SelectorStyle>
       <button onClick={() => setAspectRatio('1/1')}>1:1</button>
       <button onClick={() => setAspectRatio('9/16')}>9:16</button>
       <button onClick={() => setAspectRatio('16/9')}>16:9</button>
-    </div>
+    </SelectorStyle>
   );
 };
 
