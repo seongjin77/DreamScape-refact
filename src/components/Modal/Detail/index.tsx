9bf1bdeb99d5bf0e03f52434c70dff22ec9cbb52
규민 강 <gnto2000@naver.com>
2025-01-17 09:31:40 +0900

    fix: 이미지상세페이지 웹스타일 정리 및 모바일 작업

--- src/components/Modal/Detail/index.tsx
+++ src/components/Modal/Detail/index.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { CloseButton, ImageWrapper, ModalStyle, CommentModalStyle } from './Styled';
+import {
+  ModalContainerStyle,
+  CloseButton,
+  ImageWrapper,
+  ModalStyle,
+  CommentModalStyle,
+} from './Styled';
 import useModal from '../../../hooks/useModal';
 import { Button } from '@mui/material';
 import { useDeviceType } from '../../../hooks/useDeviceType';
@@ -54,12 +60,12 @@ const DetailImage: React.FC<DetailImageProps> = ({ imageUrl, description }) => {
   if (!imageData) return <p>데이터를 불러오는 데 실패했습니다.</p>;
 
   return (
-    <div style={{ display: 'flex', width: deviceType === 'mobile' ? '100%' : '40vw' }}>
-      <ModalStyle openComment={openComment} deviceType={deviceType}>
-        <div className="modal-header">
-          {/* 닫기 버튼 */}
-          <CloseButton onClick={() => closeModal('detailModal')}>✖</CloseButton>
-        </div>
+    <ModalContainerStyle>
+      <ModalStyle
+        className={`${deviceType === 'mobile' ? 'mobile-modal' : ''}`}
+        openComment={openComment}
+        deviceType={deviceType}
+      >
         <div className="modal-body">
           {/* 이미지와 설명 */}
           <ImageWrapper>
@@ -67,52 +73,80 @@ const DetailImage: React.FC<DetailImageProps> = ({ imageUrl, description }) => {
             <p className="description">{description}</p>
           </ImageWrapper>
           <div className="button-box">
+            {deviceType === 'mobile' ? (
+              <Button
+                className="close-btn"
+                variant="outlined"
+                onClick={() => closeModal('detailModal')}
+              >
+                닫기
+              </Button>
+            ) : (
+              ''
+            )}
             <Button variant="contained" onClick={openDescription}>
               {openComment ? '닫기' : '상세보기'}
             </Button>
           </div>
         </div>
       </ModalStyle>
-      <CommentModalStyle openComment={openComment} deviceType={deviceType}>
+      <CommentModalStyle
+        className={`${deviceType === 'mobile' ? 'mobile-modal' : ''}`}
+        openComment={openComment}
+        deviceType={deviceType}
+      >
+        {deviceType === 'mobile' ? (
+          <button className="modal-close" onClick={() => setOpenComment(false)}>
+            x
+          </button>
+        ) : (
+          ''
+        )}
         <div className="comment-add">
-          <textarea
-            onChange={(e) => {
-              setCommentValue(e.target.value);
-            }}
-          />
-          <Button variant="contained">등록하기</Button>
+          <div className="user-info-add">
+            <div className="form">
+              <label htmlFor="">닉네임</label>
+              <input type="text" />
+            </div>
+            <div className="form">
+              <label htmlFor="">패스워드</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="textarea-add">
+            <textarea
+              onChange={(e) => {
+                setCommentValue(e.target.value);
+              }}
+            />
+            <Button variant="contained">등록하기</Button>
+          </div>
         </div>
-        <ul className="comment-list">
-          <li>
-            <p>댓글내용</p>
-            <div className="button-box">
-              <Button variant="contained">수정하기</Button>
-              <Button color="error" variant="outlined">
-                삭제하기
-              </Button>
-            </div>
-          </li>
-          <li>
-            <p>댓글내용</p>
-            <div className="button-box">
-              <Button variant="contained">수정하기</Button>
-              <Button color="error" variant="outlined">
-                삭제하기
-              </Button>
-            </div>
-          </li>
-          <li>
-            <p>댓글내용</p>
-            <div className="button-box">
-              <Button variant="contained">수정하기</Button>
-              <Button color="error" variant="outlined">
-                삭제하기
-              </Button>
-            </div>
-          </li>
+        <ul className="comment-list-wrapper">
+          {Array.from({ length: 7 }).map((_, index) => (
+            <li className="comment-list" key={index}>
+              <div className="comment-user-info">
+                <span className="nickname">닉네임 {index + 1}</span>
+              </div>
+              <div className="comment-wrapper">
+                <p className="comment">
+                  댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....
+                  댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....
+                  댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....댓글내용입니다.....
+                </p>
+                {/* <span className="">더보기</span> */}
+              </div>
+              <div className="button-box">
+                <Button variant="contained">수정하기</Button>
+                <Button color="error" variant="outlined">
+                  삭제하기
+                </Button>
+              </div>
+            </li>
+          ))}
         </ul>
       </CommentModalStyle>
-    </div>
+    </ModalContainerStyle>
   );
 };
 
