9dd3bbe727a1d61c79a9ebc0570190e8e9c33786
규민 강 <gnto2000@naver.com>
2025-02-04 16:43:43 +0900

    feat: 푸터변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,11 +6,6 @@ const Footer: React.FC = () => {
     <FooterStyle>
       <footer className="footer-wrraper">
         <div className="footer-contents">
-          <div className="info-area">
-            <p>언어</p>
-            <p>서비스 약관</p>
-            <p>개인정보 처리방침</p>
-          </div>
           <div className="link-area">
             <p>support@artguru.ai</p>
             <p>© 2024 All Rights Reserved</p>
